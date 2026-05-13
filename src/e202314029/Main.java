4e894a6f21c21ccaac35ad09fbfee9ec8026f2dd
kimsieun <mkdirtory@gmail.com>
2026-05-13 10:43:43 +0900

    three

--- src/e202314029/Main.java
+++ src/e202314029/Main.java
@@ -3,5 +3,6 @@ package e202314029;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("김시은");
+		World.run();
 	}
 }
---
+++ src/e202314029/World.java
@@ -0,0 +1,7 @@
+package e202314029;
+
+public class World {
+  public static void run() {
+    System.out.println("202314029");
+  }
+}
